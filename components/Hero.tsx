b3a514f89306f1a51cc8cb3b35419acf97672155
engr-idemudia <idemudiamondayosaghae@gmail.com>
2024-12-07 02:23:38 +0100

    update layout-description, page.tsx and tailwind-config

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <h1>Hello Idemu!!</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
